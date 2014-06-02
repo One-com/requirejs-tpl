6a487b2c63884bf0527eb31597d5182e2466d07c
Andreas Lind Petersen <andreas@one.com>
2014-06-02 16:10:44 +0200

    Put the test data directly into the test suite.

--- test/tpl.js
+++ test/tpl.js
@@ -2,41 +2,42 @@ var unexpected = require('unexpected'),
     _ = require('underscore'),
     jsdom = require('jsdom'),
     sinon = require('sinon'),
-    fs = require('fs'),
     Path = require('path'),
     vm = require('vm'),
     tplPath = Path.resolve(__dirname, '..', 'lib', 'tpl.js'),
-    tplText = fs.readFileSync(tplPath, 'utf-8');
+    tplText = require('fs').readFileSync(tplPath, 'utf-8');
 
 describe('tpl', function () {
+    var window,
+        document,
+        templateName;
+    beforeEach(function () {
+        document = jsdom.jsdom();
+        window = {document: document};
+        templateName = 'theTemplateName.html';
+    });
+
     var expect = unexpected.clone()
         .installPlugin(require('unexpected-sinon'))
         .installPlugin(require('unexpected-jsdom'))
         .addAssertion('to be loaded as a template', function (expect, subject, value) {
-            if (typeof subject === 'string') {
-                subject = {name: subject};
-            }
-            var context = vm.createContext(),
-                document = subject.document || '<html><head></head><body></body></html>';
-            if (typeof document === 'string') {
-                document = jsdom.jsdom(document);
-            }
+            expect(subject, 'to be a string');
+            var context = vm.createContext();
             context.window = context;
-            context.document = document;
-            context.console = console;
-            _.extend(context, subject.context);
+            context.console = console; // For debugging
+            _.extend(context, window);
             context.define = sinon.spy(function (obj) {
                 expect(obj.load, 'to be a function');
                 var req = sinon.spy(function (deps, cb) {
-                    expect(deps, 'to equal', ['text!' + subject.name]);
+                    expect(deps, 'to equal', ['text!' + templateName]);
                     expect(cb, 'to be a function');
-                    var htmlString = fs.readFileSync(Path.resolve(__dirname, '..', 'testdata', subject.name), 'utf-8');
+                    var htmlString = subject;
                     cb(htmlString);
                 });
 
                 var req,
                     load = sinon.spy();
-                obj.load(subject.name, req, load);
+                obj.load(templateName, req, load);
                 expect(req, 'was called once');
                 expect(load, 'was called once');
             });
@@ -48,106 +49,91 @@ describe('tpl', function () {
         });
 
     it('should load a template', function () {
-        expect('pureText.html', 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="pureText">Pure text</script></body></html>');
+        expect('Pure text', 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="theTemplateName">Pure text</script></body></html>');
     });
 
     it('should not overwrite an existing template of with the same id', function () {
         expect(function () {
-            expect({
-                document: '<html><head></head><body><script type="text/html" id="pureText">Existing pure text</script></body></html>',
-                name: 'pureText.html'
-            }, 'to be loaded as a template');
-        }, 'to throw exception', 'tpl plugin for require.js: More than one of the loaded templates have the file name pureText.html, skipped pureText.html. Please disambiguate by changing at least one of the file names.');
+            document.body.innerHTML = '<script type="text/html" id="theTemplateName">Existing pure text</script>';
+            expect('Pure text', 'to be loaded as a template');
+        }, 'to throw exception', 'tpl plugin for require.js: More than one of the loaded templates have the file name theTemplateName.html, skipped theTemplateName.html. Please disambiguate by changing at least one of the file names.');
     });
 
     it('should invoke TRHTML on the template if available', function () {
-        var context = {
-            TRHTML: sinon.spy(function (element) {
-                return 'Ren tekst';
-            })
-        };
-        expect({
-            context: context,
-            name: 'pureText.html'
-        }, 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="pureText">Ren tekst</script></body></html>');
+        window.TRHTML = sinon.spy(function (element) {
+            return 'Ren tekst';
+        });
+        expect('Pure text', 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="theTemplateName">Ren tekst</script></body></html>');
 
-        expect(context.TRHTML, 'was called once');
+        expect(window.TRHTML, 'was called once');
     });
 
     it('should invoke TRHTML on the template if available and DEFAULTLOCALEID !== LOCALEID', function () {
-        var context = {
-            DEFAULTLOCALEID: 'en_us',
-            LOCALEID: 'da',
-            TRHTML: sinon.spy(function (element) {
-                return 'Ren tekst';
-            })
-        };
-        expect({
-            context: context,
-            name: 'pureText.html'
-        }, 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="pureText">Ren tekst</script></body></html>');
+        window.DEFAULTLOCALEID = 'en_us';
+        window.LOCALEID = 'da';
+        window.TRHTML = sinon.spy(function (element) {
+            return 'Ren tekst';
+        });
+        expect('Pure text', 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="theTemplateName">Ren tekst</script></body></html>');
 
-        expect(context.TRHTML, 'was called once');
+        expect(window.TRHTML, 'was called once');
     });
 
     it('should not TRHTML on the template body if TRANSLATE is false', function () {
-        var context = {
-            TRANSLATE: false,
-            TRHTML: sinon.spy(function (element) {
-                return 'Ren tekst';
-            })
-        };
-        expect({
-            context: context,
-            name: 'pureText.html'
-        }, 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="pureText">Pure text</script></body></html>');
+        window.TRANSLATE = false;
+        window.TRHTML = sinon.spy(function (element) {
+            return 'Ren tekst';
+        });
+        expect('Pure text', 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="theTemplateName">Pure text</script></body></html>');
 
-        expect(context.TRHTML, 'was not called');
+        expect(window.TRHTML, 'was not called');
     });
 
     it('should not TRHTML on the template body if LOCALEID is equal to DEFAULTLOCALEID', function () {
-        var context = {
-            DEFAULTLOCALEID: 'en_us',
-            LOCALEID: 'en_us',
-            TRHTML: sinon.spy(function (element) {
-                return 'Ren tekst';
-            })
-        };
-        expect({
-            context: context,
-            name: 'pureText.html'
-        }, 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="pureText">Pure text</script></body></html>');
+        window.DEFAULTLOCALEID = 'en_us';
+        window.LOCALEID = 'en_us';
+        window.TRHTML = sinon.spy(function (element) {
+            return 'Ren tekst';
+        });
+        expect('Pure text', 'to be loaded as a template', '<html><head></head><body><script type="text/html" id="theTemplateName">Pure text</script></body></html>');
 
-        expect(context.TRHTML, 'was not called');
+        expect(window.TRHTML, 'was not called');
     });
 
     it('should hoist nested <script type=text/html> to the top level while preserving the attributes', function () {
         expect(
-            'issue1.html',
+            '<script type="text/html" data-outer="id">' +
+                '<div data-bind="class: cls"></div>' +
+            '</script>' +
+            '<script type="text/html" data-inner="id">' +
+                '<span>Text</span>' +
+            '</script>',
             'to be loaded as a template',
             '<html>' +
-            '<head>' +
-            '</head>' +
-            '<body>' +
-            '<script type="text/html" data-outer="id">\n' +
-            '    <div data-bind="class: cls"></div>\n' +
-            '</script>' +
-            '<script type="text/html" data-inner="id">\n' +
-            '    <span>Text</span>\n' +
-            '</script>' +
-            '</body>' +
-            '</html>');
+                '<head>' +
+                '</head>' +
+                '<body>' +
+                '<script type="text/html" data-outer="id">' +
+                    '<div data-bind="class: cls"></div>' +
+                '</script>' +
+                '<script type="text/html" data-inner="id">' +
+                    '<span>Text</span>' +
+                '</script>' +
+                '</body>' +
+            '</html>'
+        );
     });
 
     it('should hoist nested templates', function () {
         expect(
-            'nestedTemplates.html',
+            'Foo bar' +
+            '<script type="text/html" id="subtemplate" foo="bar">Contents of subtemplate</script>',
             'to be loaded as a template',
             '<html>' +
                 '<head></head>' +
                 '<body>' +
                     '<script type="text/html" id="subtemplate" foo="bar">Contents of subtemplate</script>' +
-                    '<script type="text/html" id="nestedTemplates">Foo bar\n\n</script>' +
+                    '<script type="text/html" id="theTemplateName">Foo bar</script>' +
                 '</body>' +
             '</html>'
         );
